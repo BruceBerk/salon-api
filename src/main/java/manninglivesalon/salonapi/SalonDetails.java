6f0350020893853dab1b533df482e93305903bd8
Bruce Berkowicz <bruceberk@me.com>
2020-08-03 15:38:51 -0400

    Finished section 1.2 setup

--- src/main/java/manninglivesalon/salonapi/SalonDetails.java
+++ src/main/java/manninglivesalon/salonapi/SalonDetails.java
@@ -1,22 +1,29 @@
 package manninglivesalon.salonapi;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 @Data
-@ConfigurationProperties("salon")
+@Configuration
 public class SalonDetails {
 
+    @Value("${salon.name}")
     private String name;
 
+    @Value("${salon.address}")
     private String address;
 
+    @Value("${salon.city}")
     private String city;
 
+    @Value("${salon.state}")
     private String state;
 
+    @Value("${salon.zipcode}")
     private String zipcode;
 
+    @Value("${salon.phone}")
     private String phone;
 
 }
